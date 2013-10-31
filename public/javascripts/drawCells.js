a48dbd741e5f716c74085c42175435809661f49d
Matthew Weitzel <mweitzel@gmail.com>
2013-10-31 16:57:56 -0600

    separate creation of a game from its running behaviour

--- public/javascripts/drawCells.js
+++ public/javascripts/drawCells.js
@@ -1,13 +1,3 @@
-var canvas = document.getElementById('board');
-canvas.height = document.body.clientHeight - 100;
-canvas.width = canvas.height;
-var ctx = canvas.getContext("2d");
-var my_gradient = ctx.createLinearGradient(0,0,0,canvas.height);
-my_gradient.addColorStop(0,"black");
-my_gradient.addColorStop(1,"grey");
-ctx.fillStyle = my_gradient;
-
-
 var drawCell = function(x,y,width,height, border) {
   ctx.fillRect(x,y,width-border,height-border);
 };
@@ -31,6 +21,15 @@ var drawBoard = function(){
 var clearBoard = function(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
+
+var canvas = document.getElementById('board');
+canvas.height = document.body.clientHeight - 100;
+canvas.width = canvas.height;
+var ctx = canvas.getContext("2d");
+var my_gradient = ctx.createLinearGradient(0,0,0,canvas.height);
+my_gradient.addColorStop(0,"black");
+my_gradient.addColorStop(1,"grey");
+ctx.fillStyle = my_gradient;
 
 drawBoard();
 
